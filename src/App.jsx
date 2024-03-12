661b065a35975fb5a4ab1f10b8a86b9e9b371ffa
Md Sayed Hossain <freelancersayed45@gmail.com>
2024-03-12 23:08:06 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+
+import './App.css'
+import Blogs from './components/Blogs/Blogs'
+import Header from './components/Header/Header'
+
+function App() {
+
+
+  return (
+    <>
+      
+      <div className='mx-5'>
+
+          <Header></Header>
+          <Blogs></Blogs>
+
+      </div>
+     
+    </>
+  )
+} 
+
+export default App
